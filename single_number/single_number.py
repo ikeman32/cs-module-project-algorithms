8d3a040f0a49894630eb1a25add795df2ad18fcb
David Isakson <ikeman32@yahoo.com>
2020-06-17 12:38:46 -0700

    Initial commit

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,31 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    '''
+    target = arr.pop(0)
+    if target in arr: 
+        arr.pop(target) 
+    return single_number(target)
+    '''
+    try:#Try recursive method first
+        target = arr.pop(0)
+        if target in arr:
+            arr.pop(arr.index(target))
+        else:
+            return target
+        return single_number(arr)
+    except:#If and exception occurs use a while loop
+        while True:
+            target = arr.pop(0)
+        if target in arr:
+            arr.pop(arr.index(target))
+        else:
+            return target
+    
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 1, 4, 4, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")

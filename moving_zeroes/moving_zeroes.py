188cfae04b667b57aae168946c740c4a8f8bb456
David Isakson <ikeman32@yahoo.com>
2020-06-17 13:12:34 -0700

    Moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,20 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    '''
+    Move non zero integers to left order not important
+    loop through the len(arr)
+    pop and append all zeros to the end of the list
+    return the mutated list
+    '''
+    #Original solution does not pass final test but otherwise is a valid solution
+    for idx in range(len(arr)):
+        #originally is 0
+        if arr[idx] != 0:
+            #originally .append(arr.pop(idx))
+            arr.insert(0, arr.pop(idx))
 
-    pass
+    return arr
 
 
 if __name__ == '__main__':

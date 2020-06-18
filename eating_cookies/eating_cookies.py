13f39af66f6ac347ce2283692c486c9a9320ea9a
David Isakson <ikeman32@yahoo.com>
2020-06-18 10:24:43 -0700

    cache does nto work with tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,7 +2,7 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     '''
     eat n cookies x ways
     were a cookies can be eaten at one time
@@ -19,11 +19,20 @@ def eating_cookies(n):
     
     if n < 0:
         return 0
+
+    if cache is None:
+        cache = {}
+
+    if n in cache:
+        return cache[n]
+    
         
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 0
+    num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")

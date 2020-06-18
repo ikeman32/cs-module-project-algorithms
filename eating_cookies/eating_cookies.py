77550b208507b8751f4eb2bf33db03189454ba1c
David Isakson <ikeman32@yahoo.com>
2020-06-18 09:59:15 -0700

    pass first test

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,27 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    '''
+    eat n cookies x ways
+    were a cookies can be eaten at one time
+    if n == 0:
+        return 1
+    
+    if n < 0:
+        return 0
+        
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    '''
+    if n == 0:
+        return 1
+    
+    if n < 0:
+        return 0
+        
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 0
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
